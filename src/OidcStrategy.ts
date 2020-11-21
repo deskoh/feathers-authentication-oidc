9a6e36793cae544e125cf5edf9cac5ecd3997174
deskoh <deskoh.sg@gmail.com>
2020-11-21 20:06:26 +0800

    Added updateEntity flag

--- src/OidcStrategy.ts
+++ src/OidcStrategy.ts
@@ -1,4 +1,4 @@
-import { AuthenticationRequest, AuthenticationResult, JWTStrategy } from '@feathersjs/authentication';
+import { AuthenticationResult, JWTStrategy } from '@feathersjs/authentication';
 import { NotAuthenticated } from '@feathersjs/errors';
 import { Params } from '@feathersjs/feathers';
 import Debug from 'debug';
@@ -25,32 +25,38 @@ export class OidcStrategy extends JWTStrategy {
     debug(this.configuration);
   }
 
-  get entityId (): string {
-    const { entityService } = this;
-    return this.configuration.entityId || (entityService && entityService.id);
+  async handleConnection(): Promise<void> {
+    return;
   }
 
-  getEntityQuery(profile: any): any {
+  get entityId(): string {
+    return this.configuration.entityId || this.entityService?.id;
+  }
+
+  /**
+   * Get query for existing entity using JWT payload
+   */
+  getEntityQuery(decodedJwt: any): any {
     return {
-      [`${this.name}Id`]: profile.sub || profile.id
+      [`${this.name}Id`]: decodedJwt.sub || decodedJwt.id
     };
   }
 
-  getEntityData(profile: any, _params: Params): any {
-    debug('getEntityData profile', profile);
+  /**
+   * Extract data from JWT to for creating or upating of entity.
+   * @param decodedJwt
+   * @param _params
+   */
+  getEntityData(decodedJwt: any, _params: Params): any {
+    debug('getEntityData decodedJwt', decodedJwt);
     return {
-      [`${this.name}Id`]: profile.sub || profile.id,
-      email: profile.email,
-      username: profile.preferred_username
+      [`${this.name}Id`]: decodedJwt.sub || decodedJwt.id,
+      email: decodedJwt.email,
     };
   }
 
-  getProfile(_data: AuthenticationRequest, result: AuthenticationResult): any {
-    return result.authentication.payload;
-  }
-
-  async findEntity(profile: any, params: Params): Promise<any> {
-    const query = await this.getEntityQuery(profile);
+  async findEntity(decodedJwt: any, params: Params): Promise<any> {
+    const query = await this.getEntityQuery(decodedJwt);
 
     debug('findEntity with query', query);
 
@@ -65,17 +71,17 @@ export class OidcStrategy extends JWTStrategy {
     return entity;
   }
 
-  async createEntity(profile: any, params: Params): Promise<any> {
-    const data = await this.getEntityData(profile, params);
+  async createEntity(decodedJwt: any, params: Params): Promise<any> {
+    const data = await this.getEntityData(decodedJwt, params);
 
     debug('createEntity with data', data);
 
     return this.entityService.create(data, params);
   }
 
-  async updateEntity(entity: any, profile: any, params: Params): Promise<any> {
+  async updateEntity(entity: any, decodedJwt: any, params: Params): Promise<any> {
     const id = entity[this.entityId];
-    const data = await this.getEntityData(profile, params);
+    const data = await this.getEntityData(decodedJwt, params);
 
     debug(`updateEntity with id ${id} and data`, data);
 
@@ -83,38 +89,37 @@ export class OidcStrategy extends JWTStrategy {
   }
 
   async getEntity(result: any, params: Params): Promise<any> {
-    const { entityService } = this;
-    const { entityId = entityService.id, entity } = this.configuration;
+    const { entityId } = this;
 
     if (!entityId || result[entityId] === undefined) {
-      throw new NotAuthenticated('Could not get oAuth entity');
+      throw new NotAuthenticated('Could not get entity for OIDC');
     }
 
     if (!params.provider) {
       return result;
     }
 
-    return entityService.get(result[entityId], {
+    const { entity } = this.configuration;
+    return this.entityService.get(result[entityId], {
       ...params,
       [entity]: result
     });
   }
 
   async authenticate(authentication: AuthenticationResult, originalParams: Params): Promise<any> {
-    const { accessToken } = authentication;
+    const { accessToken, updateEntity = false } = authentication;
     const { entity } = this.configuration;
 
     if (!accessToken) {
       throw new NotAuthenticated('No access token');
     }
 
-    const payload = await this.verifyJwt(accessToken/*, params.jwt*/);
+    const decodedJwt = await this.verifyJwt(accessToken/*, params.jwt*/);
     const result: AuthenticationResult = {
-      // TBD: Provide truthy accessToken here to prevent feathers to generate own JWT in authentication.create
-      accessToken: true,
+      // Use a 'truthy' value for Feathers authentication to skip JWT creation `createAccessToken`
+      accessToken: 'none',
       authentication: {
-        strategy: this.name || 'unknown',
-        payload
+        strategy: this.name || 'oidc',
       }
     };
 
@@ -122,14 +127,20 @@ export class OidcStrategy extends JWTStrategy {
       return result;
     }
 
+    // Find entity using internal call by removing provider.
     const { provider, ...params } = originalParams;
-    const profile = await this.getProfile(authentication, result);
-    const existingEntity = await this.findEntity(profile, params);
+    const existingEntity = await this.findEntity(decodedJwt, params);
 
     debug('authenticate with (existing) entity', existingEntity);
 
-    const authEntity = !existingEntity ? await this.createEntity(profile, params)
-      : await this.updateEntity(existingEntity, profile, params);
+    let authEntity: any;
+    if (!existingEntity) {
+      authEntity = await this.createEntity(decodedJwt, params)
+    } else if (updateEntity) {
+      authEntity = await this.updateEntity(existingEntity, decodedJwt, params);
+    } else {
+      authEntity = existingEntity;
+    }
 
     return {
       ...result,
